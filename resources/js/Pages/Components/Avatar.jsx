61e0bf395384178a4115a929473c9c4e04abb999
Cristhian Hernandez <cristhianjhl.web@gmail.com>
2024-01-23 14:45:34 -0500

    fix react warnings on avatar component

--- resources/js/Pages/Components/Avatar.jsx
+++ resources/js/Pages/Components/Avatar.jsx
@@ -1,15 +1,24 @@
 function PlaceholderAvatar() {
     return (
-        <div class="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
-            <svg class="absolute w-12 h-12 text-gray-400 -left-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z" clip-rule="evenodd"></path></svg>
+        <div className="relative w-10 h-10 overflow-hidden bg-gray-100 rounded-full dark:bg-gray-600">
+            <svg
+                className="absolute w-12 h-12 text-gray-400 -left-1"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <path
+                    fillRule="evenodd"
+                    d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                    clipRule="evenodd"
+                ></path>
+            </svg>
         </div>
     );
 }
 
 export default function Avatar({ src, alt }) {
-    if (!src) return <PlaceholderAvatar />
+    if (!src) return <PlaceholderAvatar />;
 
-    return (
-        <img class="w-10 h-10 rounded-full" src={src} alt={alt} />
-    );
+    return <img className="w-10 h-10 rounded-full" src={src} alt={alt} />;
 }
